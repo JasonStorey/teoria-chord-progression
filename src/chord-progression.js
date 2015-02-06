be151d52c4a4e32dd6524412ec66657d13d890d5
Jason Storey <jason.andrew.storey@gmail.com>
2015-02-06 00:58:30 +0000

    Throw error if progressions array contains values not within degrees of the provided scale.

--- src/chord-progression.js
+++ src/chord-progression.js
@@ -1,16 +1,27 @@
 function ChordProgression(scale, progression) {
+    var notes;
+
     if(!scale || !progression) {
         throw new Error('Expected 2 arguments but received 0.');
     }
 
     if(!scale.notes || !scale.transpose) {
-        throw new Error('Invalid argument: TeoriaScale');
+        throw new Error('Invalid argument: TeoriaScale.');
     }
 
     if(!(progression instanceof Array) || progression.filter(function(n) { return typeof n !== 'number'; }).length > 0) {
-        throw new Error('Invalid argument: Integer Array');
+        throw new Error('Invalid argument: Integer Array.');
     }
-    // Does nothing... yet.
+
+    notes = scale.notes();
+
+    progression.forEach(function(n) {
+        var degree = notes[n - 1];
+
+        if(!degree) {
+            throw new Error('Invalid Progression: Scale has ' + notes.length + ' degrees.');
+        }
+    });
 }
 
 module.exports = ChordProgression;
--- test/teoria-chord-progression-test.js
+++ test/teoria-chord-progression-test.js
@@ -10,17 +10,37 @@ describe('Teoria Chord Progression', function() {
         });
 
         it('throws an error if TeoriaScale is not provided', function() {
-            assert.throws(function() { progression('not a scale', []); }, /Invalid argument: TeoriaScale/);
+            assert.throws(function() { progression('not a scale', []); }, /Invalid argument: TeoriaScale./);
         });
 
         it('throws an error if Array is not provided', function() {
             var scale = teoria.scale('c', 'major');
-            assert.throws(function() { progression(scale, 'not an array'); }, /Invalid argument: Integer Array/);
+            assert.throws(function() { progression(scale, 'not an array'); }, /Invalid argument: Integer Array./);
         });
 
         it('throws an error if Array contains NaNs', function() {
             var scale = teoria.scale('c', 'major');
-            assert.throws(function() { progression(scale, [1, 2, 'egg']); }, /Invalid argument: Integer Array/);
+            assert.throws(function() { progression(scale, [1, 2, 'egg']); }, /Invalid argument: Integer Array./);
+        });
+
+        it('throws an error if Array contains numbers outside of provided scale\'s range', function() {
+            var scale = teoria.scale('c', 'major'),
+                degrees = scale.notes().length;
+            assert.throws(function() { progression(scale, [1, 2, degrees + 1]); }, /Invalid Progression: Scale has 7 degrees./);
+        });
+    });
+
+    describe('with valid arguments', function() {
+        var validScale,
+            validProgression;
+
+        beforeEach(function() {
+            validScale = teoria.scale('c', 'major');
+            validProgression = [2, 5, 1];
+        });
+
+        it('does not throw', function() {
+            assert.doesNotThrow(function() { progression(validScale, validProgression); });
         });
     });
 });
